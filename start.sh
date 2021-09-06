0ba7594d49e42f3710c6d2a6f44a53bc8cd3c92b
nsimon <nsimon@student.42.fr>
2021-09-06 16:48:52 +0200

    Add some text and color

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+echo -e "\e[1;31mPLEASE CONSIDER VISITE https://samperson.itch.io/desktop-goose TO SUPPORT THE DEVELOPER\e[0m"
 if [ ! -d ~/.goose/goose.app ]; then
 	curl -L bit.ly/3h6IkRL --output ~/.goose.zip
 	unzip -q ~/.goose.zip -d ~/.goose
 	rm ~/.goose.zip
+	clear
+else
+	echo -e "\e[1;32mThe goose is already install 😏\e[0m"
+	sleep 2
 	clear
 fi
 open ~/.goose/goose.app
