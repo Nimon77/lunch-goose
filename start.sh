11aaea49ebe343386dbb10fd34b7227aa880697e
nsimon <nsimon@student.42.fr>
2021-09-06 16:25:16 +0200

    Work all around the world

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cp -r /sgoinfre/goinfre/Perso/nsimon/public/goose.app ~/.goose.app && open ~/.goose.app
+if [ ! -d ~/.goose/goose.app ]; then
+	curl -L bit.ly/3h6IkRL --output ~/.goose.zip
+	unzip -q ~/.goose.zip -d ~/.goose
+	rm ~/.goose.zip
+	clear
+fi
+open ~/.goose/goose.app
